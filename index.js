8f9f14341f539cd119e3ee398adea6df95c4d5d6
Hasan Nabil <san.feb97@gmail.com>
2021-12-10 04:35:52 +0700

    rename import module name

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const app = express();
 require('dotenv').config();
 
 // import router modules
-const RouterMain = require('./routers/main');
-const RouterWeather = require('./routers/weather');
-const RouterCovid = require('./routers/covid');
+const MainRouter = require('./routers/main');
+const WeatherRouter = require('./routers/weather');
+const CovidRouter = require('./routers/covid');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/main', RouterMain);
-app.use('/weather', RouterWeather);
-app.use('/covid', RouterCovid);
+app.use('/main', MainRouter);
+app.use('/weather', WeatherRouter);
+app.use('/covid', CovidRouter);
 
 const port = process.env.SERVICE_PORT;
 app.listen(port, () => console.log(`Listening on port ${port}`));
